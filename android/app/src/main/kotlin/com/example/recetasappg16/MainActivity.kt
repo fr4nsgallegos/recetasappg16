48f5e5a98f4ccdc0ae5bd48333e06255e7d0afa9
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-08-28 20:55:45 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/recetasappg16/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.recetasappg16
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  Widget? Homepage;
+  runApp(MaterialApp(home: Homepage, debugShowCheckedModeBanner: false));
+}
