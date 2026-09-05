5236f32d142ad6fa966eb6b046380d99c99c35f7
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-09-04 21:00:48 -0500

    materialapprouter

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,25 @@ import 'package:recetasappg16/examples/textfield_page.dart';
 import 'package:recetasappg16/examples/textformfield_page.dart';
 import 'package:recetasappg16/pages/home_page.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(home: NavigatorRoutesPage(), debugShowCheckedModeBanner: false),
+//   );
+//   // runApp(MaterialApp(home: ListviewPage(), debugShowCheckedModeBanner: false));
+// }
+
 void main() {
-  runApp(
-    MaterialApp(home: NavigatorRoutesPage(), debugShowCheckedModeBanner: false),
-  );
-  // runApp(MaterialApp(home: ListviewPage(), debugShowCheckedModeBanner: false));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "Clase GoRouter",
+    );
+  }
 }
